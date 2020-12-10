5b71304dc468d644f1f88a5031bb384762e2afa3
youngtrashbag <yuule.smeker@gmail.com>
2020-12-10 11:08:31 +0100

    feat: add placeholder functions for my app
    get_notes
    post_note

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,20 @@
+extern crate actix_web;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .service(post_note)
+            .service(get_notes)
     })
     .bind("127.0.0.1:8080")?
     .run()
     .await
 }
 
+/*
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -26,4 +28,24 @@ async fn echo(req_body: String) -> impl Responder {
 
 async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
+}
+*/
+
+#[get("/note")]
+async fn get_notes() -> impl Responder {
+    // use a logger instead of println !!
+    println!("somebody requested allda notes!");
+    let body: String = "{ \"notes\": [ {\"content\": \"yea whatever...\"}, {\"note2\": \"yes, the second note\"} ]}".to_string();
+    HttpResponse::Ok()
+    .content_type("application/json")
+    .body(body)
+}
+
+#[post("/note")]
+async fn post_note(note: String) -> impl Responder {
+    // 1. parse the note from json or whatever
+    // 2. write it to database
+    // return ok if success
+
+    HttpResponse::Ok()
 }
