43b6ee209e735182b5634f5b561f1dc2e3d95729
youngtrashbag <yuule.smeker@gmail.com>
2020-12-13 10:18:30 +0100

    chore: switch from XMLHttpRequest to axios
    axios is really cool and my code works better each day !!

--- wwwroot/src/components/note.jsx
+++ wwwroot/src/components/note.jsx
@@ -1,31 +1,20 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import "./note.css";
 
-function getNotes() {
-    const XHR = new XMLHttpRequest();
-
-    // Define what happens on successful data submission
-    XHR.addEventListener('load', function(event) {
-        alert('Yeah! Data sent and response loaded.');
-    });
-
-    // Define what happens in case of error
-    XHR.addEventListener('error', function(event) {
-        alert('Oops! Something went wrong.');
-    });
-
-    // Set up our request
-    XHR.open('GET', window.$server + '/note');
-
-    //XHR.setRequestHeader('Access-Control-Allow-Origin', '*');
-    // Add the required HTTP header for form data POST requests
-    //XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    // Finally, send our data.
-    XHR.send();
-
-    return XHR.responseText;
+async function getNotes() {
+    axios({
+        method: "get",
+        url: window.$server + '/note',
+    }).then(
+        function (res) {
+            console.log(res.data);
+            console.log(res.status);
+            console.log(res.statusText);
+            return res.data;
+        }
+    ).catch(err => console.log("Axios err: ", err));
 }
 
 class Note extends Component {
@@ -48,7 +37,7 @@ class Notes extends Component {
         var notes = getNotes();
         console.log(notes);
         notes = JSON.parse(notes);
-        var rendered = "";
+        var rendered = "<h1>h1</h1>";
 
         notes.forEach(element => {
             rendered += <Note content={element.content} author={element.author}/>
--- wwwroot/src/components/noteform.jsx
+++ wwwroot/src/components/noteform.jsx
@@ -1,45 +1,31 @@
 import React from 'react';
+import axios from 'axios';
+import qs from 'qs';
+
 import './noteform.css';
 
-function postNote(content, author) {
-    const XHR = new XMLHttpRequest();
-    let urlEncodedData = "",
-        urlEncodedDataPairs = [];
+async function postNote(content, author) {
+    axios({
+        method: "post",
+        url: window.$server + '/note',
+        data: qs.stringify({content: content, author: author}),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }).then(
+        function (res) {
+            console.log(res.data);
+        }
+    ).catch(err => console.log("Axios err: ", err));
 
-    urlEncodedDataPairs.push(encodeURIComponent("author") + '=' + encodeURIComponent(author));
-    urlEncodedDataPairs.push(encodeURIComponent("content") + '=' + encodeURIComponent(content));
-
-    // Combine the pairs into a single string and replace all %-encoded spaces to 
-    // the '+' character; matches the behaviour of browser form submissions.
-    urlEncodedData = urlEncodedDataPairs.join( '&' ).replace( /%20/g, '+' );
-
-    // Define what happens on successful data submission
-    XHR.addEventListener('load', function(event) {
-        alert('Yeah! Data sent and response loaded.');
-    });
-
-    // Define what happens in case of error
-    XHR.addEventListener('error', function(event) {
-        alert('Oops! Something went wrong.');
-    });
-
-    // Set up our request
-    XHR.open('POST', window.$server + '/note');
-
-    //XHR.setRequestHeader('Access-Control-Allow-Origin', '*');
-    // Add the required HTTP header for form data POST requests
-    XHR.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-
-    // Finally, send our data.
-    XHR.send(urlEncodedData);
 }
 
 class NoteForm extends React.Component {
     render() {
         return (
         <div class="NoteForm">
-            <input type="text" minLenght="3" maxLength="255" name="content" id="content" placeholder="please enter your note here"/>
-            <input type="text" minLenght="3" maxLength="32" name="author" id="author" placeholder="please enter thy name"/>
+            <input type="text" minLength="3" maxLength="255" name="content" id="content" placeholder="please enter your note here"/>
+            <input type="text" minLength="3" maxLength="32" name="author" id="author" placeholder="please enter thy name"/>
             <button onClick={() => postNote(document.getElementById('content').value,document.getElementById('author').value)} value="write note.">ok</button>
         </div>
         );
